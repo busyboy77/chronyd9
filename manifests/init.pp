01d09cb81b6254ac5a9c90b8b5dd76accd9dcb7e
NMR <nasir.mahmood@gmail.com>
2019-11-01 05:17:18 -0500

    Changed the init file syntax error

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,15 @@
 class chronyd9 {
 	ensure_packages(['chrony'])
-file {'/etc/chrony.conf':
-	source => 'puppet:///modules/files/chrony.conf',
-	notify => Service['chonryd'],
-	reqire => Package['chrony'],
-}
 
-Service {'chronyd':
-	ensure => running,
-	enable => true,
-}
+	file {'/etc/chrony.conf':
+		source => 'puppet:///modules/files/chrony.conf',
+		notify => Service['chonryd'],
+		require => Package['chrony'],
+	}
+
+	Service {'chronyd':
+		ensure => running,
+		enable => true,
+	}
 }
 	
